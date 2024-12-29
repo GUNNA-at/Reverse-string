4871b299e726506a31e15e70136f9fa657d9974d
GUNNA <GUNNA@DESKTOP-KS6NDOV>
2024-12-29 14:25:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string original = "Andrii Kolodii";
+        string reversed = ReverseString(original);
+        Console.WriteLine($"original: {original}");
+        Console.WriteLine($"reversed: {reversed}");
+    }   
+        public static string ReverseString(string input)
+
+    {
+        string reversed = string.Empty;
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reversed += input[i];
+        }
+        return reversed;
+
+    }
+}  
